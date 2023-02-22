7b9c963cd509c461181cabbca2fd80abc12899e8
prashan133 <dsrprasant@gmail.com>
2023-02-22 21:28:59 +0545

    delete task

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,5 +1,6 @@
 const Task = require("../models/taskModel");
 
+// create a task
 const createTask = async(req,res) => {
        // saving data to database (create)
     try {
@@ -10,6 +11,7 @@ const createTask = async(req,res) => {
     }
 }
 
+// get all task 
 const getTasks = async(req,res) =>{
      //reading the data from database
     try {
@@ -20,6 +22,7 @@ const getTasks = async(req,res) =>{
     }
 }
 
+// get a single task
 const getTask = async(req,res) => {
     // getting a single task from the database
     try {
@@ -35,6 +38,24 @@ const getTask = async(req,res) => {
     }
 }
 
+// delete a task
+
+const deleteTask = async(req,res) =>{
+    try {
+        const {id} = req.params;
+        const task = await Task.findByIdAndDelete(id);
+
+        if(!task){
+            return res.status(404).json(`No task with id: ${ id }`);
+        }
+
+        res.status(200).send("Task deleted");// we used send  methond here because i don't want back any task
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+        
+    }
+
+}
 module.exports = {
-    createTask,getTasks,getTask
+    createTask,getTasks,getTask,deleteTask
 }
--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createTask, getTasks, getTask } = require("../controllers/taskController");
+const { createTask, getTasks, getTask, deleteTask } = require("../controllers/taskController");
 const Task = require("../models/taskModel");
 const router = express.Router();
 
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post("/api/tasks" ,createTask);
 router.get("/api/tasks",getTasks);
 router.get("/api/tasks/:id" , getTask);
+router.delete("/api/tasks/:id",deleteTask);
 
 module.exports = router// so that we can import it into anthore file
